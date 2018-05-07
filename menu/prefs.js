115d18cb5db56e8e7a1e1a8a60d2d2c8d7ac4097
MarioCMFlys <mariocmflysmail@gmail.com>
2018-05-07 18:11:42 -0500

    Add options in pref menu

---
+++ menu/prefs.js
@@ -0,0 +1,58 @@
+/**
+Copyright 2018 MarioCMFlys
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**/
+
+function defCheckbox(name, def){
+  chrome.storage.sync.get(name, function(result){
+
+    if(result[name] == true){
+      document.getElementById(name).checked = "checked";
+    }
+    else if(result[name] == false){}
+    else{
+      if(def == true){
+        document.getElementById(name).checked = "checked";
+      }
+    }
+  });
+}
+function checkSet(name){
+  value = document.getElementById(name).checked;
+  f = {};
+  f[name] = value;
+  chrome.storage.sync.set(f, function(){});
+}
+// Canvas
+defCheckbox("canvasListModules", true);
+defCheckbox("canvasPeople", true);
+// Portal
+defCheckbox("portalHelp", true);
+// Experimental
+defCheckbox("canvasDarkTheme", false);
+
+document.getElementById("opt").addEventListener("submit", function(event){
+  event.preventDefault();
+
+  checkSet("canvasListModules");
+  checkSet("canvasPeople");
+
+  checkSet("portalHelp");
+
+  checkSet("canvasDarkTheme");
+
+  btn = document.getElementById("btnSave");
+  btn.innerHTML = "Saved";
+  btn.disabled = true;
+});
