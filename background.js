c0b1a61f3dc194d2b7fe38dcf70d764a0070e999
MarioCMFlys <mariocmflysmail@gmail.com>
2018-09-10 17:26:37 -0500

    Finish compatibility

--- background.js
+++ background.js
@@ -15,13 +15,5 @@ limitations under the License.
 **/
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostSuffix: ''},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+  chrome.browserAction.enable();
 });
