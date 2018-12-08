a5e4b320fcc79010a9e7f9b37604269e90bd475e
MarioCMFlys <mariocmflysmail@gmail.com>
2018-12-08 13:08:43 -0600

    Add omnibox helper

--- background.js
+++ background.js
@@ -17,3 +17,68 @@ limitations under the License.
 chrome.runtime.onInstalled.addListener(function() {
   chrome.browserAction.enable();
 });
+
+var suggestions = [
+  {s: {content: "canvas", description: "Canvas"}, url: "https://canvas.allenisd.org/"},
+  {s: {content: "skyward", description: "Skyward Gradebook"}, url: "https://skyward.allenisd.org/"},
+  {s: {content: "portal", description: "Portal"}, url: "https://portal.allenisd.org/"},
+  {s: {content: "google", description: "Google Drive"}, url: "https://google.allenisd.org/"},
+  {s: {content: "montage", description: "Safari Montage <dim>(montage)</dim>"}, url: "https://montage.allenisd.org/"},
+  {s: {content: "citrix", description: "Citrix Storefront"}, url: "https://citrix.allenisd.org/vpn/index.html"},
+  {s: {content: "home", description: "AISD Front Page <dim>(home)</dim>"}, url: "https://www.allenisd.org/"},
+  {s: {content: "hs", description: "Allen HS <dim>(hs)</dim>"}, url: "https://www.allenisd.org/allenhs"},
+  {s: {content: "lowery", description: "Lowery FC"}, url: "https://www.allenisd.org/loweryhs"},
+  {s: {content: "curtis", description: "Curtis MS"}, url: "https://www.allenisd.org/curtisms"},
+  {s: {content: "ereckson", description: "Ereckson MS"}, url: "https://www.allenisd.org/erecksonms"},
+  {s: {content: "ford", description: "Ford MS"}, url: "https://www.allenisd.org/fordms"},
+  {s: {content: "erma", description: "ERMA"}, url: "https://erma.allenisd.org/"},
+  {s: {content: "test", description: "Test Skyward <dim>(test)</dim>"}, url: "https://testskyward.allenisd.org/"},
+  {s: {content: "ticket", description: "Helpdesk <dim>(ticket)</dim>"}, url: "https://helpdesk.allenisd.org/"},
+  {s: {content: "apg", description: "Academic Planning Guide <dim>(apg)</dim>"}, url: "https://canvas.allenisd.org/courses/858742"}
+];
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+  text = text.toLowerCase();
+
+  s = [];
+  for(i=0;i<suggestions.length;i++){
+    s.push(suggestions[i].s);
+  }
+
+  current = s.filter(function(e){
+    return e.content.startsWith(text);
+  });
+
+  if(current.length >= 1){
+     chrome.omnibox.setDefaultSuggestion({description: current[0].description});
+     current.shift();
+  }
+  suggest(current);
+});
+
+function open(loc){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var tab = tabs[0];
+    chrome.tabs.update(tab.id, {url: loc});
+  });
+}
+function openQuery(q){
+  open("https://www.google.com/search?q="+encodeURIComponent("aisd "+q));
+}
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  text = text.toLowerCase();
+
+  current = suggestions.filter(function(e){
+    return e.s.content.startsWith(text);
+  });
+
+  if(current.length >= 1){
+    req = current[0];
+    open(req.url);
+
+  }
+  else{
+    openQuery(text);
+  }
+});
