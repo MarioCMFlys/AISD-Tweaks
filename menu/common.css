066098c162838dc421279c41e18a713dfbc12a47
MarioCMFlys <mariocmflysmail@gmail.com>
2020-03-05 19:45:35 -0600

    Update version to 1.8.1 and change copyright year to 2020

--- menu/common.css
+++ menu/common.css
@@ -1,5 +1,5 @@
 /**
-Copyright 2018-2019 MarioCMFlys
+Copyright 2018-2020 MarioCMFlys
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
